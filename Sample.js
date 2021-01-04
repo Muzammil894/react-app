ac38d02944c70b5153b3842040f869344ec2369f
Muzammil894 <53493215+Muzammil894@users.noreply.github.com>
2021-01-04 14:06:25 +0530

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,16 @@
+//import Modal from './Modal';
+//import useModal from './useModal';
+
+//const Sample = () => {
+  //const {isShowing, toggle} = useModal();
+  //return (
+    //<div className="Sample">
+      //<button className="button-default" onClick={toggle}>Show Modal</button>
+      //<Modal
+        //isShowing={isShowing}
+        //hide={toggle}
+      ///>
+    //</div>
+  //);
+//};
+//export default Sample;
